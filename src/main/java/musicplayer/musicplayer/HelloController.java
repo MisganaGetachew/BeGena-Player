98c8cfe51398d7f702fc5e6728abd3ea9588cadc
Samuel Negasa <samuelnegasa2@gmail.com>
2023-12-18 07:32:31 -0800

    I've created media control buttons(play, pause, next and previous buttons) and give functionality for those buttons. Clicking each button triggers the associated action, resulting in console messages.

--- src/main/java/musicplayer/musicplayer/HelloController.java
+++ src/main/java/musicplayer/musicplayer/HelloController.java
@@ -11,4 +11,23 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    void playButton(){
+        System.out.println("Play button clicked!");
+    }
+    @FXML
+    void pauseButton(){
+        System.out.println("pause button clicked!");
+    }
+
+    @FXML
+    void previousButton(){
+        System.out.println("previous button clicked!");
+    }
+
+    @FXML
+    void nextButton(){
+        System.out.println("next button clicked!");
+    }
 }
