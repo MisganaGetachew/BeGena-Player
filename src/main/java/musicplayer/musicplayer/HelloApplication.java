db60b8f751c71d4bbdb6003109026992b5dacaf8
MisganaGetachew <misganagetachew111@gmail.com>
2023-12-18 14:59:01 +0300

    added separate tabs for d/t functionalities

--- src/main/java/musicplayer/musicplayer/HelloApplication.java
+++ src/main/java/musicplayer/musicplayer/HelloApplication.java
@@ -12,9 +12,14 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Music Player");
+        stage.setHeight(500);
+        stage.setWidth(600);
         stage.setScene(scene);
+        stage.setX(100);
+        stage.setY(50);
         stage.show();
+
     }
 
     public static void main(String[] args) {
