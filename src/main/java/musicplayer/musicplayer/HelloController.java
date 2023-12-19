29e5f81b630a3ca9074af489acb609e61727bdb6
MisganaGetachew <misganagetachew111@gmail.com>
2023-12-19 17:12:51 +0300

    added choose_file btn functionality

--- src/main/java/musicplayer/musicplayer/HelloController.java
+++ src/main/java/musicplayer/musicplayer/HelloController.java
@@ -3,6 +3,10 @@ package musicplayer.musicplayer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
 public class HelloController {
     @FXML
     private Button btn1;
@@ -18,6 +22,21 @@ public class HelloController {
 @FXML
     void playMusic(){
     System.out.println("play music is cliked");
+}
+
+@FXML
+    void choose_file(){
+
+    FileChooser chosen = new FileChooser();
+    File selectedFile = chosen.showOpenDialog(null);
+//    System.out.println("file chooser selected ");
+
+    if (selectedFile != null){
+        System.out.println(selectedFile.getName());
+    }
+    else{
+        System.out.println("file can't be found");
+    }
 }
 
 }
